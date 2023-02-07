0e382d2c7673a74e9de6ec783f5cfdcfbaa1ab16
Rebecca Kelly <bk@ancilla.ca>
2023-02-07 17:14:43 -0500

    add seek and ateof to iostreams

--- iostream.lua
+++ iostream.lua
@@ -1,11 +1,15 @@
 -- Stream IO for the nandgame VM.
--- An iostream device is backed by a file (or at least by a character device)
--- and takes up two words in memory.
--- Reads and writes to the first word will return successive words from the
--- backing stream, or write words to it.
--- Reads and writes to the second are identical, but will read and write bytes --
--- on writes the high byte is ignored and on reads the high byte is always 0x00.
--- Word-sized reads/writes are BIG-ENDIAN regardless of the host platform.
+-- An iostream device is backed by a file (or at least by a character device).
+-- It maps three addresses:
+-- 0 - file status word
+--      read: 0x0001 if there is data remaining in the file, 0x0000 otherwise
+--      write: seek to the written offset (absolute from start of file)
+-- 1 - byte-width IO
+--      read: return the next byte from the file. The high bits of the word will be 0.
+--      write: write a single byte to the file. The input will be & with 0x00FF.
+-- 2 - word-width IO
+--      read: return the next 16-bit word from the file, MSB first.
+--      write: write a 16-bit word to the file, MSB first.
 
 local bit = require "bit"
 
@@ -20,30 +24,36 @@ function iostream:__tostring()
   return string.format("iostream(%s, %s)", self.path, self.mode)
 end
 
--- two words of memory
-function iostream:size() return 2 end
+-- Three words: [status, bytes, words]
+function iostream:size() return 3 end
 
 -- IO functions
 function iostream:read(address)
   if address == 0 then
+    -- status bit
+    return self.fd:read(0) and 0x0001 or 0x0000
+  elseif address == 1 then
+    -- read byte
+    return self.fd:read(1):byte()
+  elseif address == 2 then
     -- read word
     local high,low = self.fd:read(2):byte(1,2)
     return high * 256 + low
-  else
-    -- read byte
-    return self.fd:read(1):byte()
   end
 end
 
 function iostream:write(address, data)
   if address == 0 then
+    -- seek to offset
+    self.fd:seek('set', data)
+  elseif address == 1 then
+    -- write byte
+    self.fd:write(string.char(data % 256))
+  elseif address == 2 then
     -- write word
     local low = data % 256
     local high = (data/256) % 256
     self.fd:write(string.char(high, low))
-  else
-    -- write byte
-    self.fd:write(string.char(data % 256))
   end
 end
 
