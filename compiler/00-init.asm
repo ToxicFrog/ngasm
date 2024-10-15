54e67c4ad67a3551c2b5286d8d51d7ecea52e58a
Rebecca Kelly <bk@ancilla.ca>
2024-10-15 19:12:05 -0400

    Typo and comment cleanup

--- compiler/00-init.asm
+++ compiler/00-init.asm
@@ -1,21 +1,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 0_init.asm
-;; Variable declarations and initialization routines.
+;; 00-init.asm
+;; Program entry point.
 ;; The name starts with 0_ so that when concatenating the various asm files
 ;; to get the input to the assembler, this comes first.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Since each line outputs an instruction, we can use labels for our variables
-; as well. In the future we'll have define but this works in the meantime.
-; The convention used here is:
-; - variable addresses start with :&
-; - constants that are not memory addresses with :#
-; - jump labels with plain :
-
-
-; Memory-mapped IO we still need to hard-code until we have define, using labels
-; as variables only works for stuff where we don't care exactly where it ends up.
-
-; Entry point
+; Unconditionally jump to the actual entrypoint.
+; This lets us put whatever other code between here and Init that we want.
 @ :Init.
 = 0+D <=>
--- compiler/zz-postscript.asm
+++ compiler/zz-postscript.asm
@@ -1,1 +1,4 @@
+; This both acts as a pointer into RAM used for the symbol table when compiling,
+; and as a marker for the start of the symbol table in the generated compiler
+; ROM, which is useful when debugging.
 :&symbols.
