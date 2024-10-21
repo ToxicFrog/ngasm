2aeec5d8fb7ec71e27e5aa1748feaed41d2c6140
Rebecca Kelly <bk@ancilla.ca>
2024-10-20 21:03:41 -0400

    Add function call tests

---
+++ tests/functions.lua
@@ -0,0 +1,72 @@
+local tests = ...
+
+-- pushd, pusha, pushm, pushconst, pushvar
+function tests.call1(test)
+  test:source [[
+    :Return_Five
+    ~function, 0
+    ~pushconst, 5
+    ~return
+
+    :Init
+    ~stack/init, $1000
+    ~call, :Return_Five, 0
+  ]]
+
+  test:check_ram(
+    '&SP', 0x1001,
+    0x1001, { 5 }
+  )
+end
+
+function tests.add(test)
+  test:source [[
+    :Add
+    ~function, 2
+    ~popd
+    @ &SP
+    A = M-1
+    D = D+M
+    ~drop
+    ~pushd
+    ~return
+
+    :Init
+    ~stack/init, $1000
+    ~pushconst, $0055
+    ~pushconst, $3300
+    ~call, :Add, 2
+  ]]
+
+  test:check_ram(
+    '&SP', 0x1001,
+    0x1001, { 0x3355 }
+  )
+end
+
+function tests.recurse(test)
+  test:source [[
+    :Recur
+    ~function, 1
+    ~popd
+    @ :Recur_Zero
+    D = D-1 <=
+    ~pushd
+    ~call, :Recur, 1
+    ~jmp, :Recur_End
+    :Recur_Zero
+    ~pushconst, 99
+    :Recur_End
+    ~return
+
+    :Init
+    ~stack/init, $1000
+    ~pushconst, 5
+    ~call, :Recur, 1
+  ]]
+
+  test:check_ram(
+    '&SP', 0x1001,
+    0x1001, { 99 }
+  )
+end
